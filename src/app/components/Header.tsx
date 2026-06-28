7414ca98fde68812db34864aa724f48461a3d128
Jashanpreet Singh <jas123hanpreet@gmail.com>
2026-06-28 19:25:39 +0530

    fix:

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
+import { cn } from '../utils/cn'
 import Image from "next/image"
 import bgMainImg from "@/assets/bg-masthead.jpg"
 
 export default function Header() {
   return (
-        <header className='relative min-h-100 max-h-150 w-full flex flex-col justify-center items-center p-2'>
+        <header className={cn(
+            'relative min-h-100 max-h-150 w-full',
+            'flex flex-col justify-center items-center', 
+            'p-2')}>
             <Image
               src={bgMainImg} 
               fill 
               alt="bg image"
-              className="w-screen max-h-170 z-0 object-cover"
+              className={cn('w-screen max-h-170 z-0', 'object-cover')}
             />
-            <div className="relative flex flex-col h-full w-full z-10 text-white text-5xl justify-center items-center">
-                <h1 className='text-center'>Build a landing page for your business or project and generate more leads!</h1>
+            <div className={cn(
+            'relative h-full w-full z-10', 
+            'flex flex-col justify-center items-center',
+            'text-white text-5xl')}>
+                <h1 className={cn('text-center')}>Build a landing page for your business or project and generate more leads!</h1>
             </div>
         </header>
     )
