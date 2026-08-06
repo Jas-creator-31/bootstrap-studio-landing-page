70a94771dfc721244e0add5422dc1bd448cdcd05
jashan <jas123hanpreet@gmail.com>
2026-08-06 16:52:46 +0530

    fix: made output standalone to make prodoction enviornment minimal

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  allowedDevOrigins: ["test.jashanpreet.me"]
+  allowedDevOrigins: ["test.jashanpreet.me"],
+  output: "standalone",
 };
 
 export default nextConfig;
