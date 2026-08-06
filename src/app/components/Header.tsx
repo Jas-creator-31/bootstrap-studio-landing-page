891ee23cb3252e3cea3cb80b8ecf6ba0c0cbb071
jashan <jas123hanpreet@gmail.com>
2026-08-06 20:31:01 +0530

    made text size and font

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,20 +7,25 @@ export default function Header() {
   return (
         <header className={cn(
             'relative min-h-100 max-h-150 w-full',
-            'flex flex-col justify-center items-center', 
+            'flex flex-col justify-center items-center',
             'p-2')}>
             <Image
-              src={bgMainImg} 
-              fill 
+              src={bgMainImg}
+              fill
               alt="bg image"
               className={cn('w-screen max-h-170 z-0', 'object-cover')}
             />
             <div className={cn(
-            'relative h-full w-full z-10', 
-            'flex flex-col justify-center items-center',
-            'text-white text-5xl')}>
+            'relative h-full w-full z-10',
+              'flex flex-col justify-center items-center',
+              '',
+            'text-white text-3xl font-semibold')}>
                 <h1 className={cn('text-center')}>Build a landing page for your business or project and generate more leads!</h1>
             </div>
-        </header>
+
+            <div className={cn('')}>
+                i
+            </div>
+    </header>
     )
 }
