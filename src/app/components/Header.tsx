ec050949438ef95e8ada931a4ebfb8c2876bf1a4
jashan <jas123hanpreet@gmail.com>
2026-08-09 12:39:54 +0530

    fix: removed buggy random height and width classes
    thay were making elements behave in unpredictible ways and were not
    needed at all in the first place

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -22,7 +22,7 @@ export default function Header() {
       />
       <div
         className={cn(
-          "relative h-full w-full z-10",
+          "relative z-10",
           "flex flex-col justify-center items-center gap-4",
           "",
           "text-white text-3xl font-semibold",
@@ -30,16 +30,15 @@ export default function Header() {
       >
         <h1 className={cn("text-center whitespace-pre-line sm:whitespace-normal")}>{mainTest}</h1>
         <div className={cn(
-          "h-max w-full max-w-96 sm:w-lg z-10",
+          "z-10",
           "flex flex-col md:flex-row items-center gap-2 md:gap-10",
           "p-1",
         )}>
           <div className={cn(
-            "h-max w-full",
             "flex flex-row items-center justify-center md:justify-end"
           )}>
             <input type="email" className={cn(
-              "h-11 w-full",
+              "w-full",
               "p-3 rounded bg-white",
               "text-gray-900 placeholder:text-neutral-900/60",
               "caret-transparent focus:caret-black",
@@ -50,11 +49,9 @@ export default function Header() {
           </div>
 
           <div className={cn(
-            "h-max w-full",
             "flex flex-row items-center justify-center md:justify-start"
           )}>
             <button className={cn(
-              "h-10 w-30",
               "align-middle",
               "p-2 rounded-md bg-blue-400",
               "text-white text-lg font-normal",
