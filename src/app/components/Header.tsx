a7e43f98edd7383803b4280a3009a958118895be
jashan <jas123hanpreet@gmail.com>
2026-08-09 13:00:03 +0530

    fix: added some comments for better readability

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,20 +7,20 @@ export default function Header() {
   const mainTest =
     "Build a landing page\n for your business or\n project and generate more leads!";
   return (
-    <header
+    <header // main header element that holds all the other elements for the components
       className={cn(
         "relative min-h-100 max-h-150 w-full",
         "flex flex-col justify-center items-center",
         "p-2",
       )}
     >
-      <Image
+      <Image // background image component that will be the main background for the header section
         src={bgMainImg}
         fill
         alt="bg image"
         className={cn("w-screen max-h-170 z-0", "object-cover")}
       />
-      <div
+      <div // the content container for the header section
         className={cn(
           "relative z-10",
           "flex flex-col justify-center items-center gap-4",
@@ -28,13 +28,18 @@ export default function Header() {
           "text-white text-3xl font-semibold",
         )}
       >
-        <h1 className={cn("text-center whitespace-pre-line sm:whitespace-normal")}>{mainTest}</h1>
-        <div className={cn(
+
+        <h1 // the heading text for the header section
+          className={cn("text-center whitespace-pre-line sm:whitespace-normal")}
+        >{mainTest}
+        </h1>
+
+        <div className={cn( // the secondary container for containers of email input field and sign up button
           "z-10",
           "flex flex-col md:flex-row items-center gap-2 md:gap-10",
           "p-1",
         )}>
-          <div className={cn(
+          <div className={cn( // the container for the email input field
             "flex flex-row items-center justify-center md:justify-end"
           )}>
             <input type="email" className={cn(
@@ -48,7 +53,7 @@ export default function Header() {
             />
           </div>
 
-          <div className={cn(
+          <div className={cn( // the container for the sign up button
             "flex flex-row items-center justify-center md:justify-start"
           )}>
             <button className={cn(
