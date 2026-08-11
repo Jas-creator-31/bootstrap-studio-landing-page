6890099b3fdb59dc9646e02ee353ee14597cebf1
jashan <jas123hanpreet@gmail.com>
2026-08-11 17:53:07 +0530

    fix: redused padding by 1

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -43,7 +43,7 @@ export default function Header() {
           )}>
             <input type="email" className={cn( // email input field
               "w-full sm:w-md md:w-lg",
-              "p-3 rounded bg-white",
+              "p-2 rounded bg-white",
               "text-gray-900 placeholder:text-neutral-900/60",
               "caret-transparent focus:caret-black",
               "text-lg font-normal"
