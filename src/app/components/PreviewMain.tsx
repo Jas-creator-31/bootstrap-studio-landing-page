ba227f776461686d305a3a92bb4ad8b7da9addc8
jashan <jas123hanpreet@gmail.com>
2026-08-14 16:43:40 +0530

    feat: added other 2 previews as well

--- src/app/components/PreviewMain.tsx
+++ src/app/components/PreviewMain.tsx
@@ -1,5 +1,7 @@
 import Preview from "@/app/components/Preview"
 import screenIcon from "@/assets/screen.svg"
+import layersIcon from "@/assets/layers.svg"
+import okIcon from "@/assets/ok.svg"
 
 
 export default function PreviewMain() {
@@ -7,9 +9,17 @@ export default function PreviewMain() {
   const screenIconHeading = "Fully Responsive"
   const screenIconParagraph = "This theme will look great on any device, no matter the size!"
 
+  const layersIconHeading = "Bootstrap 5 Ready"
+  const layersIconParagraph = "Featuring the latest build of the new Bootstrap 5 framework!"
+
+  const okIconHeading = "Easy to Use"
+  const okIconParagraph = "Ready to use with your own content, or customize the source files!"
+
   return (
     <div>
       <Preview icon={screenIcon} heading={screenIconHeading} peragraph={screenIconParagraph} />
+      <Preview icon={layersIcon} heading={layersIconHeading} peragraph={layersIconParagraph} />
+      <Preview icon={okIcon} heading={okIconHeading} peragraph={okIconParagraph} />
     </div>
   )
 }
