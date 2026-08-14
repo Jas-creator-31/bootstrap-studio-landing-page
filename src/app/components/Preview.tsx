d80aa0c1325a7581290f7e3f91503466cbe8e27d
jashan <jas123hanpreet@gmail.com>
2026-08-14 20:15:15 +0530

    fix: made text align center

--- src/app/components/Preview.tsx
+++ src/app/components/Preview.tsx
@@ -28,7 +28,7 @@ export default function Preview({Icon, heading, peragraph}: Props) {
         className="text-lg font-bold"
       >{heading}</h1>
       <p // text
-        className="text-wrap font-light"
+        className="text-center text-wrap font-light"
       >{peragraph}</p>
     </div>
   )
