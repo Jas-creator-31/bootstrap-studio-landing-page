3786da1b88d8055997e8c9662c310d3bdae0872a
jashan <jas123hanpreet@gmail.com>
2026-08-19 18:52:49 +0530

    fix: added heading and added a container for main testimoniesSection

--- src/app/components/Testimonies.tsx
+++ src/app/components/Testimonies.tsx
@@ -28,12 +28,21 @@ export default function Testimonies() {
 
   return (
     <section className={cn(
-        "w-full",
-        "flex flex-col lg:flex-row items-center justify-center",
+      "w-full",
+      "flex flex-col items-center justify-center gap-16",
+      "pt-20 pb-20 pr-20 pl-20"
     )}>
-      <TestimoniesSection {...testimony1} />
-      <TestimoniesSection {...testimony2} />
-      <TestimoniesSection {...testimony3} />
+      <h1 className="text-2xl lg:text-3xl font-bold text-neutral-800 dark:text-neutral-200">
+        What people are saying...
+      </h1>
+      <div className={cn(
+        "w-full",
+        "flex flex-col lg:flex-row items-center justify-center gap-16"
+      )}>
+        <TestimoniesSection {...testimony1} />
+        <TestimoniesSection {...testimony2} />
+        <TestimoniesSection {...testimony3} />
+      </div>
     </section>
   )
 }
