e51cc019435b693d0ecb98605cccbab5014d46a7
jashan <jas123hanpreet@gmail.com>
2026-08-19 12:06:42 +0530

    feat: remade basic structure

--- src/app/components/MainSection.tsx
+++ src/app/components/MainSection.tsx
@@ -8,8 +8,25 @@ interface MainSectionProps {
   rowReverse?: boolean,
 }
 
-export default function MainSection({image, heading, paragraph, rowReverse}: MainSectionProps) {
+export default function MainSection({ image, heading, paragraph, rowReverse }: MainSectionProps) {
   return (
-
+    <section className={cn(
+      "w-full h-auto sm:max-h-200 lg:max-h-120",
+      "grid grid-cols-1 grid-rows-2 lg:grid-cols-2 lg:grid-rows-1",
+      "items-center justify-between"
+    )}>
+      <div className={cn(
+        "w-full h-full",
+        rowReverse && "lg:order-1"
+      )}>
+        <Image src={image} alt={heading}
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div>
+        <h2>{heading}</h2>
+        <p>{paragraph}</p>
+      </div>
+    </section>
   )
 }
