796d3850a31be6654c0269bf71296e07f235b573
jashan <jas123hanpreet@gmail.com>
2026-08-21 11:38:54 +0530

    fix: added dots and gave them styhless

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,11 +4,14 @@ export default function Footer() {
   return (
     <footer className="flex flex-row flex-wrap justify-between items-center p-20">
       <div>
-        <li className="text-blue-500 underline flex flex-row flex-wrap justify-center items-center gap-8">
-          <ul>About</ul>
-          <ul>Contact</ul>
-          <ul>Terms of Use</ul>
-          <ul>Privacy Policy</ul>
+        <li className="flex flex-row flex-wrap justify-center items-center">
+          <ul className="text-blue-500 underline">About</ul>
+          <span className="flex items-center justify-center text-neutral-800 text-center p-3 h-full">.</span>
+          <ul className="text-blue-500 underline">Contact</ul>
+          <span className="flex items-center justify-center text-neutral-800 text-center p-3 h-full">.</span>
+          <ul className="text-blue-500 underline">Terms of Use</ul>
+          <span className="flex items-center justify-center text-neutral-800 text-center p-3 h-full">.</span>
+          <ul className="text-blue-500 underline">Privacy Policy</ul>
         </li>
         <p>© Brand 2026. All Rights Reserved.</p>
       </div>
