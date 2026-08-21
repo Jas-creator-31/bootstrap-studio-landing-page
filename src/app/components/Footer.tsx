269a15fc3aaed75120f335db0b9e3e370d435f6c
jashan <jas123hanpreet@gmail.com>
2026-08-21 12:02:47 +0530

    refactor:

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,8 +1,13 @@
 import {Facebook, Instagram, Twitter} from "react-bootstrap-icons"
 
+import { cn } from "@/app/utils/cn"
+
 export default function Footer() {
   return (
-    <footer className="flex flex-col lg:flex-row justify-center lg:justify-between items-center p-10 gap-8">
+    <footer className={cn(
+      "flex flex-col lg:flex-row justify-center lg:justify-between items-center",
+      "p-10 gap-8"
+    )}>
       <div className="flex flex-col flex-wrap justify-center items-center">
         <li className="flex flex-row flex-wrap justify-center items-center">
           <ul className="text-blue-500 underline p-3">About</ul>
@@ -12,7 +17,11 @@ export default function Footer() {
         </li>
         <p className="text-neutral-700 dark:text-neutral-300 text-center">© Brand 2026. All Rights Reserved.</p>
       </div>
-      <div className="text-blue-500 w-full lg:max-w-1/2 flex flex-row flex-wrap justify-center lg:justify-end items-center gap-6">
+      <div className={cn(
+        "w-full lg:max-w-1/2",
+        "flex flex-row flex-wrap justify-center lg:justify-end items-center gap-6",
+        "text-blue-500"
+      )}>
         <Facebook className="h-8 w-8" />
         <Twitter className="h-8 w-8" />
         <Instagram className="h-8 w-8" />
