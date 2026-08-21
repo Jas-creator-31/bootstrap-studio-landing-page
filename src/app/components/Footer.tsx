a4c7b420309460184420465e1b0b6425c1130c34
jashan <jas123hanpreet@gmail.com>
2026-08-21 11:55:37 +0530

    fix:

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,8 +2,8 @@ import {Facebook, Instagram, Twitter} from "react-bootstrap-icons"
 
 export default function Footer() {
   return (
-    <footer className="flex flex-row flex-wrap justify-between items-center p-20">
-      <div>
+    <footer className="flex flex-col lg:flex-row justify-center lg:justify-between items-center p-10 gap-8">
+      <div className="flex flex-col flex-wrap justify-center items-center">
         <li className="flex flex-row flex-wrap justify-center items-center">
           <ul className="text-blue-500 underline">About</ul>
           <span className="flex items-center justify-center text-neutral-800 text-center p-3 h-full">.</span>
@@ -15,7 +15,7 @@ export default function Footer() {
         </li>
         <p>© Brand 2026. All Rights Reserved.</p>
       </div>
-      <div className="text-blue-500 w-full flex flex-row flex-wrap justify-center items-center gap-6 mt-8">
+      <div className="text-blue-500 w-full lg:max-w-1/2 flex flex-row flex-wrap justify-center lg:justify-end items-center gap-6">
         <Facebook className="h-8 w-8" />
         <Twitter className="h-8 w-8" />
         <Instagram className="h-8 w-8" />
